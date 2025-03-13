d87bb17a460367784476b7804ee0c741d4bb7c51
POOJITH MENDEM <94035082+poojithM@users.noreply.github.com>
2025-03-13 10:56:44 -0400

    main.py

--- main.py
+++ main.py
@@ -30,17 +30,42 @@ class audio_transcriber(Flow[Transcript]):
     def transcrbing_audio(self):
         print("Audio: ")
         path = "C:\\Users\\mpooj\\OneDrive\\Desktop\\gen_ai\\hotel_management\\src\\hotel_management\\crews\\hotel_manager\\output.mp3"
-        
+
+        #If Audio file less than 25 MB use the below code
         with open(path, "rb") as audio_file:
             transcription = client.audio.transcriptions.create(
             model="whisper-1",
             file=audio_file,
             response_format="text"  # Options: "text", "verbose_json", "srt"
         )
-        
-         
+
         self.transcript = transcription
         print(f"Transcription: {self.transcript}")
+
+        #If audio file is more than 25MB use the below code
+        """
+        chunk_length_ms = 60000
+        chunks = make_chunks(audio, chunk_length_ms)
+        
+        
+        full_transcribe = ""
+        for i, chunk in enumerate(chunks):
+            print(f"Transcribing chunk {i+1}/{len(chunks)}")
+            chunk_path = f"chunk_{i}.mp3"
+            chunk.export(chunk_path, format="mp3")
+            
+            with open(chunk_path, "rb") as audio_file:
+                transcription = client.audio.transcriptions.create(
+                   model = "whisper-1",
+                   file = audio_file 
+                )
+                full_transcribe += transcription.text + " "
+         
+        self.state.transcript = full_transcribe
+        print(f"Transcription: {self.state.transcript}")
+
+        """
+         
 
     @listen(transcrbing_audio)
     def input_transcribe(self):
